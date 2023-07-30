21335c90f69b3857597146cfe1a1e8aedb49c7b6
André carvalho <andre.vrlcarvalho@hotmail.com>
2023-07-29 22:08:59 -0300

    StrapiRealState types

---
+++ src/shared-typed/strapi-real-state.ts
@@ -0,0 +1,20 @@
+export type StrapiRealState = {
+  data: {
+    id: number;
+    attributes: {
+      name: string;
+      slug: string;
+      streetName: string;
+      neighborhood: string;
+      city: string;
+      size: string;
+      createdAt: string;
+      bedrooms: number;
+      bathrooms: number;
+      parkingSpaces: number;
+      streetNumber: string;
+      addressType: string;
+      notes: string;
+    };
+  };
+};
