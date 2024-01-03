84d5299024bf76b6190fd38e3243aed78d72541b
André Carvalho@note <andre.vrlcarvalho@hotmail.com>
2024-01-03 20:22:21 +0000

    reajustes

--- src/components/SectionBackground/styles.ts
+++ src/components/SectionBackground/styles.ts
@@ -5,14 +5,14 @@ type Background = {
 };
 
 const containerBackgroundActivate = (theme: DefaultTheme) => css`
-  background: ${theme.colors.primaryColor};
+  background: ${theme.colors.primary};
   color: ${theme.colors.white};
 `;
 
 export const Container = styled.div<Background>`
   ${({ theme, background }) => css`
     background: ${theme.colors.white};
-    color: ${theme.colors.primaryColor};
+    color: ${theme.colors.primary};
     ${!!background && containerBackgroundActivate(theme)};
     min-height: 100vh;
     display: flex;
