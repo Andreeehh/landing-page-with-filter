4a83030ecc82a40769c394a72d207a93998a3096
André Carvalho@note <andre.vrlcarvalho@hotmail.com>
2024-01-03 12:20:52 +0000

    progress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,18 @@ import { AppProps } from 'next/app';
 import '../../public/assets/fonts/styles.css';
 import { GlobalStyles } from '../styles/global-styles';
 import { BlogThemeProvider } from 'contexts/BlogThemeContext';
+import NextNprogress from 'nextjs-progressbar';
+import { theme } from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <BlogThemeProvider>
+      <NextNprogress
+        color={theme.colors.secondary}
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={10}
+      />
       <Component {...pageProps} />
       <GlobalStyles />
     </BlogThemeProvider>
